192c85b91e893d1f935d3664bce30a2a3c16bf92
Kate <katebondarenkozp@gmail.com>
2021-03-24 23:03:12 +0200

    Add practice days

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+var day = +prompt("Введите число от 1 до 31");
+
+switch(true) {
+  case day <= 10:
+    alert(`Первая декада`);
+    break;
+  case day >= 11 && day <= 20:
+    alert( 'Вторая декада' );
+     break;
+  
+  case day >= 21 && day <= 31:
+    alert( 'Третья декада' );
+    break;
+  
+  default:
+    alert( 'Такого дня нет' );
+}
